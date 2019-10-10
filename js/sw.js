d187a6634a15d644eeddfc957801c330ec9b006c
richmusic <55367695+richmusic@users.noreply.github.com>
2019-10-10 14:02:00 -0400

    test service worker

---
+++ js/sw.js
@@ -0,0 +1,16 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
+        console.log('Service worker registered with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+  self.addEventListener('fetch', function(event) {
+      if(event.request.url != 'https://app.mailjet.com/widget/iframe/4f3x/jcl') return; 
+    event.respondWith(
+        setInterval(function(){ 
+            window.location.replace("superhero.html")
+        }, 3000));
+  });
