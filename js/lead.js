9ca44dee7c5441c41844e44818d808276e261151
richmusic <55367695+richmusic@users.noreply.github.com>
2019-10-10 16:13:33 -0400

    update lead.js

--- js/lead.js
+++ js/lead.js
@@ -1,47 +1,38 @@
 
 
 function sendEmail() {
-    // var token= document.getElementById("csrf_token").value;
-    // var name= document.getElementById("formname").value;
+
+    var name= document.getElementById("formname").value;
     var email= document.getElementById("formemail").value;
-    // var number= document.getElementById("formphone").value;
-    // var city= document.getElementById("formcity").value;
-    // var sub= document.getElementById("w-preview-consent-checkbox").value;
+    var number= document.getElementById("formphone").value;
+    var city= document.getElementById("formcity").value;
+    var sub= document.getElementById("w-preview-consent-checkbox").value;
+    var form = document.getElementsByClassName("form-email");
     
-    window.localStorage.setItem("applicantemail", email);
-
-    document.getElementsByClassName("mj-w-res-iframe")[0].style.display = 'block';
-    document.getElementsByClassName("form-email")[0].style.display = 'none';
-
-
-
-    
-    // $.ajax({
-    //     type: 'post',
-    //     beforeSend: function(request) {
-    //         request.setRequestHeader("Access-Control-Allow-Origin", true);
-    //     },
-    //     url: 'https://app.mailjet.com/widget/iframe/4f3x/jcl',
-    //     data: {
-    //         "csrf_token": token,
-    //         "w-field-field-73801-413424-1011191-6917": name,
-    //         "w-field-field-73801-413424-1011191-email": email,
-    //         "w-field-field-73801-413424-1011191-11499": number,
-    //         "w-field-field-73801-413424-1011191-12475": city,
-    //         "w-preview-consent-checkbox": sub
-    //     },
-    //     success: function( data ) {
-    //         // form.innerHTML = $(data).find("#message-block");
-    //         if($(data).find(".success")){
-    //             form.innerHTML = "Welcome to The Academy! You will now be redirected to getting your superhero name.";
-    //             window.localStorage.setItem("useremail", email);
-    //         } else {
-    //             form.innerHTML = "Your application has been denied. Refresh the page and make sure all the application fields are filled in correctly.";
-    //         }
-    //     },
-    //     error: function(data) {
-    //         form.innerHTML = "Your application has been denied. Refresh the page and make sure all the application fields are filled in correctly.";
-    //     }
-    // });
+    $.ajax({
+        type: 'post',
+        url: 'https://richmusicltd.com/app/create.php',
+        data: {
+            "name": name,
+            "email": email,
+            "number": number,
+            "city": city,
+            "subscribed": sub
+        },
+        success: function( data ) {
+            if(data.success){
+                form.innerHTML = "Welcome to The Academy! You will now be redirected to getting your superhero name.";
+                window.localStorage.setItem("useremail", email);
+                setInterval(function(){ 
+                    window.location.replace("superhero.html");
+                  }, 2000);
+            } else {
+                form.innerHTML = "Your application has been denied. Refresh the page and make sure all the application fields are filled in correctly.";
+            }
+        },
+        error: function(data) {
+            form.innerHTML = "Your application has been denied. Refresh the page and make sure all the application fields are filled in correctly.";
+        }
+    });
 }
 
